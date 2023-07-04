74fb0c8bee217d84cdb9d8cb853051add7cb8849
katarighe <m.ighe@hotmail.com>
2023-07-04 15:50:25 +0300

    Refactor the JS from objects/arrays to JS classes

--- index.js
+++ index.js
@@ -1,63 +1,52 @@
-// Make sure if the local storage in empty than add an empty array
-if (localStorage.getItem('Added Books') == null) {
-  localStorage.setItem('Added Books', JSON.stringify([]));
-}
-
-// Store the books data into the local storage
-const storeData = JSON.parse(localStorage.getItem('Added Books'));
-
-function updateData() {
-  localStorage.setItem('Added Books', JSON.stringify(storeData));
-}
-
-// Retriving values from input fields
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  const title = document.querySelector('.title');
-  const author = document.querySelector('.author');
-  e.preventDefault();
-  // eslint-disable-next-line no-use-before-define
-  addNewdata(title.value, author.value);
-});
-
-function createBooks(arr) {
-  let books = '';
-  for (let i = 0; i < arr.length; i += 1) {
-    books += ` 
-          <p>${arr[i].title}</p>
-          <p>${arr[i].author}</p>
-          <button onclick ="removeBook(${i})">Remove</button>
-          <hr/>
-          `;
+class BookCollection {
+  // Make sure if the local storage in empty than add an empty array
+    static getStoredBooks() { 
+      if (localStorage.getItem('Added books') === null) {
+        localStorage.setItem('Added books', JSON.stringify([]));
+      }
+      return JSON.parse(localStorage.getItem('Added books'));
+    }
+  
+    // Store the books data into the local storage
+    static updateStoredBooks(books) {
+      localStorage.setItem('Added books', JSON.stringify(books));
+    }
+  
+    static addNewBook(bookTitle, bookAuthor) {
+      const storedBooks = BookCollection.getStoredBooks();
+  
+      const newBook = {
+        title: bookTitle,
+        author: bookAuthor,
+      };
+  
+      storedBooks.push(newBook);
+      BookCollection.updateStoredBooks(storedBooks);
+    }
+  
+    static removeBook(i) {
+      const storedBooks = BookCollection.getStoredBooks();
+      storedBooks.splice(i, 1);
+      BookCollection.updateStoredBooks(storedBooks);
+      BookCollection.displayBooks(); 
+    }
+  
+    // Displaying the books on the UI from localStorage
+    static displayBooks() {
+      const listOfBooks = document.querySelector('.container');
+      const storedBooks = BookCollection.getStoredBooks();
+      const bookListHTML = BookCollection.createBookListHTML(storedBooks);
+      listOfBooks.innerHTML = `
+        <ul class="book-ul">${bookListHTML}</ul>
+      `;
+    }
   }
-  return books;
-}
-
-// Displaying the books on the UI from localStorage
-function displayBooks() {
-  const listedBooks = document.querySelector('.container');
-  listedBooks.innerHTML = `
-      <ul class = "book-ul"/>
-      ${createBooks(storeData)}</ul>`;
-}
-
-// Adding new books to the local storage
-function addNewdata(bookTitle, bookAuthor) {
-  const Book = {
-    title: bookTitle,
-    author: bookAuthor,
-  };
-  storeData.push(Book);
-  updateData();
-  displayBooks();
-}
-
-// Removing data from the localStorage
-// eslint-disable-next-line no-unused-vars
-function removeBook(i) {
-  storeData.splice(i, 1);
-  updateData();
-  displayBooks();
-}
-
-window.onload = displayBooks;
+  
+  // Get values from input fields
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    const title = document.querySelector('.title');
+    const author = document.querySelector('.author');
+    e.preventDefault();
+    BookCollection.addNewBook(title.value, author.value);
+  });
